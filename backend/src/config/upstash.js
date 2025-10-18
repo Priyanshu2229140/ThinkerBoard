5d5e22e688190691fa0ecb5e8291d9bc33768281
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-10-18 12:33:52 +0530

    feat(ratelimit): integrate Upstash rate limiting and Redis connection

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,17 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Connect to Redis using environment variables
+const redis = Redis.fromEnv();
+
+// Create a rate limiter: 10 requests per 10 seconds
+export const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  analytics: true,
+});
+
+export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "../config/db.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import ratelimiter from "./middleware/rateLimitter.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
+
 app.use(express.json());
+app.use(ratelimiter)
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {

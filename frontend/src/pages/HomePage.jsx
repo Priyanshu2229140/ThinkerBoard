abeff14f9c0664d842e13bc23b55161fed38b1ae
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-10-27 00:23:17 +0530

    feat: add NoteCard component for displaying individual notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar.jsx";
 import RateLimitterUI from "../components/RateLimitterUI.jsx";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import NoteCard from "../components/NoteCard.jsx";
+
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
@@ -42,9 +44,11 @@ const HomePage = () => {
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <div>
-                {note.title} | {note.content}
-              </div>
+              // <div>
+              //   {note.title} | {note.content}
+              // </div>
+
+              <NoteCard key={note._id} note={note}/>
             ))}
           </div>
         )}

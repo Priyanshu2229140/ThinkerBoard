b2019839191105ea023616abe87a01c9953813aa
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-10-30 19:49:58 +0530

    feat: update theme to 'valentine' across the application and adjust NoteCard border color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import HomePage from "./pages/HomePage";
 
 const App = () => {
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", "forest");
+    document.documentElement.setAttribute("data-theme", "valentine");
   }, []);
 
   return (
     <div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,_#fff_40%,_#f9a8d4_100%)]"></div>
+
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -22,7 +22,7 @@ const NoteCard = ({ note, setNotes }) => {
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-purple-600"
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-pink-300"
     >
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
